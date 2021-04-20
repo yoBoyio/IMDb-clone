55ca7c0d4c115727ea53b42863ba6e2fc0dfafbc
Michael Theocharis <michaeltheochar@gmail.com>
2021-04-20 23:41:05 +0300

    Updated SeachPage
    Αλλαξα το Row.css ωστε να φαινονται μεγαλυτερες οι ταινιες, μετα απο καθε search

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,10 +1,39 @@
 import React from "react";
 import { connect } from "react-redux";
+import MovieCard from "../components/movies/MovieCard";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+  main: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginLeft: "30px",
+    marginRight: "30px",
+    padding: "100px",
+    width: "100",
+    justifyContent: "center",
+    alignItems: "center",
+    maxHeight: "20",
+  },
+  test: {
+    width: "20%",
+    height: "10%",
+  },
+}));
 function SearchPage(props) {
+  const classes = useStyles();
   return (
-    <div>
-      <p>Kazakopolos</p>
+    <div className={classes.main}>
+      {props.movies.map((movie) => (
+        <div>
+          <MovieCard
+            movie={movie}
+            key={movie.id}
+            image={movie.poster_path}
+          ></MovieCard>
+        </div>
+      ))}
     </div>
   );
 }
