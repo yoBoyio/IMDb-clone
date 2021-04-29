dd8bd0d71539d74f51fa1ec10a4d4d1cc8e6d875
kwstaspexli <kostaspechli2@gmail.com>
2021-04-29 22:28:47 +0300

    credits (cast)
    add react-elastic-carousel
    and some more css

--- frontend/src/components/movies/ContentModal.js
+++ frontend/src/components/movies/ContentModal.js
@@ -143,7 +143,9 @@ function ContentModal({ children, id, auth }) {
                   {/*==============LIKES & WATCHLIST ====================*/}
                   <div className="actions">
                     {auth && auth.isAuthenticated ? (
+                      <CommentModal movieId={id} >
                       <Like movieId={id} auth={true} />
+                      </CommentModal>
 
                     ) : (
                       <AuthModal>
