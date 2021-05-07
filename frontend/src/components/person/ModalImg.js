bbf5d71e9fcfab09df187c2f1111885c88b39599
Dpontika <73525083+Dpontika@users.noreply.github.com>
2021-05-07 21:21:46 +0300

    Person
    Person detail page, Popular people page

---
+++ frontend/src/components/person/ModalImg.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { makeStyles, withTheme } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    width:"315px",
+    height:"465px",
+    backgroundColor: "#39445a",
+    border: '1px solids #282c34',
+    borderRadius: 10,
+    color: "white",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(1, 1, 1),
+  },
+}));
+
+export default function ModalImg({children}) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <button type="button" style={{backgroundColor: "#8b4db5"}} onClick={handleOpen}>
+        {children}
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            {children}
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
---
+++ frontend/src/components/person/PersonInfo.js
@@ -0,0 +1,70 @@
+import React, { Component, useState } from "react";
+import ModalImg from "./ModalImg";
+import FilmTable from "./FilmTable";
+import "./PersonInfo.css";
+
+const ReadMore = ({ children }) => {
+  const text = children;
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+
+  return (
+    <p className="text">
+      {text}
+      <span onClick={toggleReadMore} className="read-or-hide">
+        {isReadMore ? "...read more" : " show less"}
+      </span>
+    </p>
+  );
+};
+
+const PersonInfo = (props) => {
+  return (
+    <div className="wrapper">
+      <div className="profile_image">
+        <ModalImg>
+          {" "}
+          <img src={`${props.image}`} alt="enlarged pic" />{" "}
+        </ModalImg>
+        <span className="infoPI">
+          <div>
+            {" "}
+            <p></p> <h4> Personal Info</h4> <b> Known For </b>{" "}
+            <p> {props.personKnown} </p>{" "}
+          </div>
+          <div>
+            {" "}
+            <b> Birthday </b> <p> {props.personBirthday} </p>{" "}
+          </div>
+          <div>
+            {" "}
+            <b> Place of Birth </b> <p> {props.personBirthPlace} </p>{" "}
+          </div>
+          <div>
+            <b>Also Known As</b>
+            {props.personAlias &&
+              props.personAlias.map((person, index) => (
+                <p> {props.personAlias[index]} </p>
+              ))}
+          </div>
+        </span>
+      </div>
+      <div className="bio">
+        <h2 className="namePI">{props.personName} </h2>
+        <p> </p>
+        <h4> Biography </h4>
+        <ReadMore>
+          {" "}
+          <p> {props.personBio}</p>{" "}
+        </ReadMore>
+        <p> </p>
+      </div>
+
+      {/* <div > {props.personHomepage} </div> */}
+    </div>
+  );
+};
+
+export default PersonInfo;
